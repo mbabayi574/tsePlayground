6afbd4d6fdaed328ac702515e4464022c777f972
Bobby <m.babayi574@gmail.com>
2026-07-27 14:27:33 +0330

    Update sys.path to use '/content' for compatibility in training script

--- finetune_csv/train_sequential.py
+++ finetune_csv/train_sequential.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import torch.distributed as dist
 
-sys.path.append('/home/bobby/Documents/tsePlayground')
+sys.path.append('/content')
 from model import Kronos, KronosTokenizer, KronosPredictor
 
 from config_loader import CustomFinetuneConfig
